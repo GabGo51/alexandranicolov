721eac2387c2c1bae49e3a3dae6c2b61304795f6
GabrielGosselin <snow.gabo51@gmail.com>
2024-01-10 16:55:20 -0500

    added serie 1 image

--- src/components/Photo/1/Serie1.js
+++ src/components/Photo/1/Serie1.js
@@ -1,9 +1,44 @@
-import React from 'react'
-
+import React from "react";
+import styled from "styled-components";
+import s1_1 from "./img/s1-1.jpg";
+import s1_2 from "./img/s1-2.jpg";
+import s1_3 from "./img/s1-3.jpg";
+import s1_4 from "./img/s1-4.jpg";
+import s1_5 from "./img/s1-5.jpg";
+import s1_6 from "./img/s1-6.jpg";
+import s1_7 from "./img/s1-7.jpg";
+import s1_8 from "./img/s1-8.jpg";
+import s1_9 from "./img/s1-9.jpg";
+import s1_10 from "./img/s1-10.jpg";
+import s1_11 from "./img/s1-11.jpg";
+import s1_12 from "./img/s1-12.jpg";
+import s1_13 from "./img/s1-13.jpg";
+import s1_14 from "./img/s1-14.jpg";
+import s1_15 from "./img/s1-15.jpg";
 const Serie1 = () => {
   return (
-    <div>Serie1</div>
-  )
-}
+    <Container>
+      <img alt="serie1" src={s1_1} />
+      <img alt="serie1" src={s1_2} />
+      <img alt="serie1" src={s1_3} />
+      <img alt="serie1" src={s1_4} />
+      <img alt="serie1" src={s1_5} />
+      <img alt="serie1" src={s1_6} />
+      <img alt="serie1" src={s1_7} />
+      <img alt="serie1" src={s1_8} />
+      <img alt="serie1" src={s1_9} />
+      <img alt="serie1" src={s1_10} />
+      <img alt="serie1" src={s1_11} />
+      <img alt="serie1" src={s1_12} />
+      <img alt="serie1" src={s1_13} />
+      <img alt="serie1" src={s1_14} />
+      <img alt="serie1" src={s1_15} />
+    </Container>
+  );
+};
 
-export default Serie1
+const Container = styled.div`
+width: 90%;
+`;
+
+export default Serie1;
