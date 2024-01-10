a4a33b3561ce465461a40b6a9c188848aef72092
GabrielGosselin <snow.gabo51@gmail.com>
2024-01-10 17:22:15 -0500

    added pic in all photo series

--- src/components/Photo/2/Serie2.js
+++ src/components/Photo/2/Serie2.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import s2_1 from "./img/s2-1.jpg";
+import s2_2 from "./img/s2-2.jpg";
+import s2_3 from "./img/s2-3.jpg";
+import s2_4 from "./img/s2-4.jpg";
+import s2_5 from "./img/s2-5.jpg";
+import s2_6 from "./img/s2-6.jpg";
+import s2_7 from "./img/s2-7.jpg";
+import styled from 'styled-components';
+
 
 const Serie2 = () => {
   return (
-    <div>Serie2</div>
+    <Container>
+      <img alt="serie1" src={s2_1} />
+      <img alt="serie1" src={s2_2} />
+      <img alt="serie1" src={s2_3} />
+      <img alt="serie1" src={s2_4} />
+      <img alt="serie1" src={s2_5} />
+      <img alt="serie1" src={s2_6} />
+      <img alt="serie1" src={s2_7} />
+
+    </Container>
   )
 }
+
+const Container = styled.div`
+width: 90%;
+`
 
 export default Serie2
--- src/components/Photo/3/Serie3.js
+++ src/components/Photo/3/Serie3.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import s3_1 from "./img/s3-1.jpg";
+import s3_2 from "./img/s3-2.jpg";
+import s3_3 from "./img/s3-3.jpg";
+import s3_4 from "./img/s3-4.jpg";
+import s3_5 from "./img/s3-5.jpg";
+import s3_6 from "./img/s3-6.jpg";
+import s3_7 from "./img/s3-7.jpg";
+import styled from 'styled-components';
 
 const Serie3 = () => {
   return (
-    <div>Serie3</div>
+    <Container>
+      <img alt="serie1" src={s3_1} />
+      <img alt="serie1" src={s3_2} />
+      <img alt="serie1" src={s3_3} />
+      <img alt="serie1" src={s3_4} />
+      <img alt="serie1" src={s3_5} />
+      <img alt="serie1" src={s3_6} />
+      <img alt="serie1" src={s3_7} />
+
+    </Container>
   )
 }
+const Container = styled.div`
+width: 90%;
+`
 
 export default Serie3
