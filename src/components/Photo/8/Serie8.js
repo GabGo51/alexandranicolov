ef9921bac312155f6b810d72d0734ca3fe596077
GabrielGosselin <snow.gabo51@gmail.com>
2024-01-12 11:46:33 -0500

    did serie 8

--- src/components/Photo/8/Serie8.js
+++ src/components/Photo/8/Serie8.js
@@ -10,19 +10,55 @@ import s8_6 from "./img/s8-6.jpg";
 const serie8 = () => {
   return (
     <Container>
-      <img alt="serie1" src={s8_1} />
-      <img alt="serie1" src={s8_2} />
-      <img alt="serie1" src={s8_3} />
-      <img alt="serie1" src={s8_4} />
-      <img alt="serie1" src={s8_5} />
-      <img alt="serie1" src={s8_6} />
-      
+      <div className="row">
+        <img className="row-img" alt="serie1" src={s8_1} />
+        <img className="row-img" alt="serie1" src={s8_2} />
+      </div>
+
+      <img className="middle" alt="serie1" src={s8_3} />
+      <img className="middle" alt="serie1" src={s8_4} />
+      <img className="middle" alt="serie1" src={s8_5} />
+
+      <div className="row last">
+        <img className="row-img" alt="serie1" src={s8_6} />
+      </div>
     </Container>
-  )
-}
+  );
+};
 
 const Container = styled.div`
-width: 90%;
+  width: 90%;
+
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .middle {
+    width: 80%;
+    margin-bottom: 4vw;
+    @media (max-width: 700px) {
+      width: 100%;
+    }
+  }
+
+  .row {
+    .row-img {
+      @media (max-width: 700px) {
+        width: 100%;
+        margin-bottom: 4vw;
+      }
+    }
+    @media (max-width: 700px) {
+      flex-direction: column;
+    }
+  }
+
+  .last {
+    justify-content: center;
+    
+  }
 `;
 
-export default serie8
+export default serie8;
